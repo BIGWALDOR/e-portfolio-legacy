f72ea6d7aa885d86b3b6f81ddaf5a8226f77de34
Walid Bouladam <bouladam.walid86@gmail.com>
2022-09-21 21:19:49 +1200

    Import Header, ScrollIcon and MailIcon components

--- personal-portfolio-react/src/App.jsx
+++ personal-portfolio-react/src/App.jsx
@@ -1,9 +1,15 @@
+import { Header } from './components/Header'
 import { Navbar } from './components/Navbar'
+import { ScrollIcon } from './components/ScrollIcon'
+import { MailIcon } from './components/MailIcon'
 
 const App = () => {
   return (
     <section id="landing-page" onmousemove="moveBackground(event)">
       <Navbar />
+      <Header />
+      <ScrollIcon />
+      <MailIcon />
     </section>
   )
 }

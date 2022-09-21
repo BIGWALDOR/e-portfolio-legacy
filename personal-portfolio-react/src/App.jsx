d9358e9c5e97b13384b755ca3804c677f01f609e
Walid Bouladam <bouladam.walid86@gmail.com>
2022-09-21 21:30:54 +1200

    Import Projects component

--- personal-portfolio-react/src/App.jsx
+++ personal-portfolio-react/src/App.jsx
@@ -2,15 +2,21 @@ import { Header } from './components/Header'
 import { Navbar } from './components/Navbar'
 import { ScrollIcon } from './components/ScrollIcon'
 import { MailIcon } from './components/MailIcon'
+import { Projects } from './components/Projects'
 
 const App = () => {
   return (
-    <section id="landing-page" onmousemove="moveBackground(event)">
-      <Navbar />
-      <Header />
-      <ScrollIcon />
-      <MailIcon />
-    </section>
+    <>
+      <section id="landing-page" onmousemove="moveBackground(event)">
+        <Navbar />
+        <Header />
+        <ScrollIcon />
+        <MailIcon />
+      </section>
+      <section id="projects">
+        <Projects />
+      </section>
+    </>
   )
 }
 

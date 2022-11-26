58bd7faa722a414b47e9961971887959b7104044
Walid Bouladam <bouladam.walid86@gmail.com>
2022-11-26 23:13:39 +1300

    Refactor into TS

--- helper/toggleModal.js
+++
@@ -1,9 +0,0 @@
-let isModalOpen = false
-
-function toggleModal() {
-  isModalOpen = !isModalOpen
-    ? (document.body.classList += ' modal--open')
-    : document.body.classList.remove('modal--open')
-}
-
-export { toggleModal }
